58baa7cc3597b6596364ed5a45c17e6ed9665fe1
Abdullah Balıkçı <abdblk2525@gmail.com>
2022-10-28 09:28:10 +0300

    Operators

---
+++ Operator/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace operatorler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //  Atama ve İşlemler
+            int x = 3;
+            int y = 2;
+
+            y = 2 + 2;
+            y += 2; // İşlemli atama denir.
+            y /= 2;
+
+        //Mantıksal Öperatörler
+            //||, &&,!
+
+            bool isSucces = true;
+            bool isComploted = false;
+
+
+            if(isSucces && isComploted)
+            {
+                Console.WriteLine("Doğru");
+            }
+
+            if(isSucces || isComploted)
+            {
+                Console.WriteLine("Doğru");
+            }
+
+
+            //  İlişkisel Öperatörler
+            // <, >, ==,<=,>=,!=
+
+
+            //Aritmetik
+            // +, -, *, /, %, ++, --
+
+            int x1 = 4;
+            int x2 = 2;
+
+            int result = x1 - x2;
+
+            result = x1 * x2;
+            result = x1++;
+
+            //Mod alma
+
+            int result2 = 10 % 3;
+            Console.WriteLine(result2);
+
+
+        }
+
+    }
+
+}
